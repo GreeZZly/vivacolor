b56906637bba57528507707f9d88bff1eea0f73f
Ruslan <jokagent@gmail.com>
2014-01-27 18:07:29 +0400

    cfsavfas

--- application/views/main/consult.php
+++ application/views/main/consult.php
@@ -8,9 +8,9 @@
 		</div>
 		<div id="consult_content_info">
 			<div id="consult_content_info_rel">
-				<div class="consult_change" itid="1">Стоит немного подумать, прежде чем отправлять на свалку старую мебель. Можно попробовать "вписать" ее в стиль своего интерьера, например, заново покрасив. Ничего не удается придумать? Тогда стоит внимательно посмотреть по сторонам - почему бы не спросить пару идей и советов у специалистов.</div>
-				<div class="consult_change" itid="2" style="display:none;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-				<div class="consult_change" itid="3" style="display:none;">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</div>
+				<div class="consult_change" itid="1">Цвета красок для стен, потолков, деревянного покрытия, бетона, загородных домов, обоев. Хотите получить персональную программу цветового дизайна прямо сейчас и решить для себя, что подходит именно Вам?</div>
+				<div class="consult_change" itid="2" style="display:none;">Стоит немного подумать, прежде чем отправлять на свалку старую мебель. Можно попробовать "вписать" ее в стиль своего интерьера, например, заново покрасив. Ничего не удается придумать? Тогда стоит внимательно посмотреть по сторонам - почему бы не спросить пару идей и советов у специалистов.</div>
+				<div class="consult_change" itid="3" style="display:none;">Для того, чтобы правильно выбрать краску, всегда требуется профессиональный совет. Какую краску выбрать для потолка? А какую для стен? и др. Как ответить на все вопросы? Здесь может помочь только профессионал.</div>
 			</div>
 			<div class="icon_con_arrow"></div>	
 			<div class="consult_button"><a href="/main/form_php">ОСТАВЬТЕ ЗАЯВКУ</a></div>	
