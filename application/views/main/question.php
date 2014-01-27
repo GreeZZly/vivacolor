71556b826f10b947ba66b7b0fa7be059f7d5e0d4
Ruslan <jokagent@gmail.com>
2014-01-27 12:38:29 +0400

    Fat Hairy Man is here

--- application/views/main/question.php
+++ application/views/main/question.php
@@ -4,5 +4,6 @@
 		<div>Оставьте заявку, наши менеджеры вам ответят.</div>
 		<div class="question_button"><a href="/main/form_php">ОСТАВЬТЕ ЗАЯВКУ</a></div>
 		<div id="question_small">или позвоните нам по номеру 8 (8352) 63-63-59</div>
+		<div id="fathairyman"></div>
 	</div>
 </div>
