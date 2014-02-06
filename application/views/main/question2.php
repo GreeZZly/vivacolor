9e24c4d1592c6abb56bc8409704a525715eeab8e
Egor Kosolapov <greezzly7@gmail.com>
2014-02-06 12:23:42 +0400

    fatman

--- application/views/main/question2.php
+++ application/views/main/question2.php
@@ -4,6 +4,6 @@
 		<div>Хватит, пора жить красиво. Раскрасьте свои мечты!!!</div>
 		<div class="question_button"><a href="/main/form_php">ОСТАВЬТЕ ЗАЯВКУ</a></div>
 		<div id="question_small">или позвоните нам по номеру 8 (8352) 63-63-59</div>
-		<div id="fathairyman"></div>
+		<div id="fathairyman"><img src="/include/images/fathairyman.png"></div>
 	</div>
 </div>
